700d88c11852e1c08e82baadfe38e320106e7c3b
kiminya-raphael <kiminyaraphael@gmail.com>
2020-06-16 07:43:57 +0300

    Add files via upload

---
+++ code/fastai_extensions/exp/imports.py
@@ -0,0 +1,10 @@
+import torch
+import numpy as np
+from fastai.vision import *
+from fastai.basic_train import *
+from fastai.metrics import error_rate
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.functional")
+from IPython.display import display, HTML
+display(HTML("<style>.container { width:100% !important; }</style>"))
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
